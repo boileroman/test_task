dd85ae419768dff8d1497f2ef7c1f1672e1cef93
Sattarov Roman <romz.2003@mail.ru>
2024-12-24 19:43:42 +0500

    revert

--- src/js/config/constants.js
+++
@@ -1,9 +0,0 @@
-const paths = {
-  headerPath: "./src/components/header/header",
-  footerPath: "./src/components/footer/footer",
-  heroPath: "./src/components/hero/hero",
-  contentPath: "./src/components/content/content",
-  clientsPath: "./src/components/clients/clients",
-};
-
-export { paths };
